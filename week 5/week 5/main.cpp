75b5f3c025c06e4f504803ad9a1e5cab0c693d2e
kekwxd <78672158+kekwxd@users.noreply.github.com>
2024-10-21 13:29:34 +0300

    Update main.cpp

--- week 5/week 5/main.cpp
+++ week 5/week 5/main.cpp
@@ -5,12 +5,18 @@
 //  Created by Mert Güngör on 21.10.2024.
 //
 
+
+// MATH LİBRARY AND  CLASSES
+
+
 #include <iostream>
 using namespace std;
 #include <cmath>
+/*
 
 int main() {
-    // insert code here...
+    // insert code here..
+    
     cout << abs(-1) << endl;
     
     cout << sqrt(6) << endl;
@@ -18,4 +24,28 @@ int main() {
     cout << pow(3, 2) << endl;
     
     return 0;
+}
+
+int main(){
+    string s = "hello";
+    int len = s.length();
+    cout << len;
+    string t = "theater";
+    int lenn = t.length();
+    string r = s.substr(0, 3); // the
+    r = s.substr(1, 4); // is now heat
+    t = s.substr(3 ,3); // t is ate r is still heat
+    t = s.substr(2);
+    
+    
+}
+         
+*/
+int main(){
+    string s = "I am the eggman he is too";
+    int k = s.find("I"); // k is 0
+    k = s.find("he"); // k is 6
+    k = s.find("egg"); // k is 9
+    k = s.rfind("he"); // k is 17
+    cout << s.find("cat"); // output is 4294...
 }
